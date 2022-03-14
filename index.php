778c6ae29b0e6f0ffdb741c6c550f658386c3e0f
daw20miva <78873278+daw20miva@users.noreply.github.com>
2022-03-14 18:22:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <li>
+    <a href="eskaerak/index.php">Eskaerak</a>
+    <a href="produktuak/index.php">Produktuak</a>
+    <a href="bezeroak/index.php">Bezeroak</a>
+    <a href="stock/index.php">Stock</a>
+</li>
+</body>
+</html>
